67d9b5fd1dd3f27b618e58517a8bdc9d22833fe5
Luis Colomé <hello@luiscolome.com>
2021-09-27 16:50:52 +0200

    Add new SVG logo
    Adde new SVG logo and header nav logo on hidden side nav.

--- inc/site-logo.php
+++ inc/site-logo.php
@@ -12,8 +12,8 @@
  * Remplazamos el título del sitio por código HTML personalizado.
  */
 function personal_genesis_seo_title( $titulo ) {
-    $titulo_front = '<h1 itemprop="headline" class="site-title"><a title="' . get_bloginfo('name') . '" href="' . get_bloginfo('url') . '"><img src="'. get_stylesheet_directory_uri() .'/assets/images/logo.svg" alt="' . get_bloginfo('name') . '"></a></h1>';
-    $titulo = '<p itemprop="headline" class="site-title"><a title="' . get_bloginfo('name') . '" href="' . get_bloginfo('url') . '"><img src="'. get_stylesheet_directory_uri() .'/assets/images/logo.svg" alt="' . get_bloginfo('name') . '"></a></p>';
+    $titulo_front = '<h1 itemprop="headline" class="site-title"><a title="' . get_bloginfo('name') . '" href="' . get_bloginfo('url') . '"><img class="site-title__image" src="'. get_stylesheet_directory_uri() .'/assets/images/logo.svg" alt="' . get_bloginfo('name') . '"></a></h1>';
+    $titulo = '<p itemprop="headline" class="site-title"><a title="' . get_bloginfo('name') . '" href="' . get_bloginfo('url') . '"><img class="site-title__image" src="'. get_stylesheet_directory_uri() .'/assets/images/logo.svg" alt="' . get_bloginfo('name') . '"></a></p>';
 	if( is_front_page() ) {
         return $titulo_front;
     }else{
