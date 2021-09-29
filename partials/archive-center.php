bc42b035d885adffbd07b1b7c19a099bf4751abf
Luis Colomé <hello@luiscolome.com>
2021-09-29 12:12:52 +0200

    Update featured image sizes

--- partials/archive-center.php
+++ partials/archive-center.php
@@ -11,13 +11,13 @@
 
 echo '<article class="lcm-center">'; 
 
-	echo '<a class="lcm-center__image-link" href="' . get_permalink() . '" tabindex="-1" aria-hidden="true">' . get_the_post_thumbnail( get_the_ID(), 'lcm-featured-images' ) . '</a>';
+	echo '<a class="lcm-center__image-link" href="' . get_permalink() . '" tabindex="-1" aria-hidden="true">' . get_the_post_thumbnail( get_the_ID(), 'lcm-center-featured' ) . '</a>';
 
 	echo '<header class="lcm-center__header">';
 		// $categories = get_the_category();
 		// $category = !empty( $categories ) ? esc_html( $categories[0]->name ) : '' ;
 		// echo '<p class="lcm-center__meta">' . $category . '</p>';
-		ea_entry_category();
+		// ea_entry_category();
  		echo '<h2 class="lcm-center__title"><a href="' . get_permalink() . '">' . get_the_title() . '</a></h2>';
 	echo '</header>';
 
--- partials/archive.php
+++ partials/archive.php
@@ -11,14 +11,19 @@
 
 echo '<article class="lcm-post">'; 
 
-	echo '<a class="lcm-post__image-link" href="' . get_permalink() . '" tabindex="-1" aria-hidden="true">' . get_the_post_thumbnail( get_the_ID(), 'lcm-featured-images' ) . '</a>';
+    echo '<figure class="lcm-post__image"';
+	    echo '<a class="lcm-post__image__link" href="' . get_permalink() . '" tabindex="-1" aria-hidden="true">' . get_the_post_thumbnail( get_the_ID(), 'lcm-center-featured' ) . '</a>';
+        ea_entry_category();
+    echo '</figure>';
 
 	echo '<header class="lcm-post__header">';
 		// $categories = get_the_category();
 		// $category = !empty( $categories ) ? esc_html( $categories[0]->name ) : '' ;
 		// echo '<p class="lcm-post__meta">' . $category . '</p>';
-		ea_entry_category();
  		echo '<h2 class="lcm-post__title"><a href="' . get_permalink() . '">' . get_the_title() . '</a></h2>';
+        echo '<p class="lcm-post__content">';
+            echo wp_trim_words( get_the_content(), 19, '...' );
+        echo '</p>';
 		echo '<a class="lcm-post__read-more-link" href="' . get_permalink() . '" tabindex="-1" aria-hidden="true">Read More<span class="screen-reader-text"> of ' . get_the_title() . '</span></a>';
 	echo '</header>';
 
