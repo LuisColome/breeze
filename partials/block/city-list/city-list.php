655099d9be1ad2a4608f02998eeed5f67c1b43e2
Luis Colomé <hello@luiscolome.com>
2021-09-30 13:06:48 +0200

    Add support for provinces and cities custom blocks

--- partials/block/city-list/city-list.php
+++ partials/block/city-list/city-list.php
@@ -2,11 +2,15 @@
 
 $terms = get_terms( array(
     'taxonomy'      => 'city',
-    'hide_empty'    => true,
+    'hide_empty'    => false,
 ) );
 ?>
-    <ul>
-        <?php foreach ($terms as $term){ ?>
-        <li><a href="<?php echo get_term_link( $term->slug, $term->taxonomy ); ?>"><?php echo $term->name; ?></a></li>
-        <?php } ?>
-    </ul>
+<div class="canadacities">
+    <div class="canadacities__wrap">
+        <ul class="canadacities__list">
+            <?php foreach ($terms as $term){ ?>
+            <li class="canadacities__item"><a class="canadacities__link" href="<?php echo get_term_link( $term->slug, $term->taxonomy ); ?>"><?php echo $term->name; ?></a></li>
+            <?php } ?>
+        </ul>
+    </div>
+</div>
--- partials/block/province-list/province-list.php
+++ partials/block/province-list/province-list.php
@@ -2,11 +2,16 @@
 
 $terms = get_terms( array(
     'taxonomy' => 'province',
-    'hide_empty' => true,
+    'hide_empty' => false,
 ) );
 ?>
-    <ul>
-        <?php foreach ($terms as $term){ ?>
-        <li><a href="<?php echo get_term_link( $term->slug, $term->taxonomy ); ?>"><?php echo $term->name; ?></a></li>
-        <?php } ?>
-    </ul>
+<div class="canadaprovinces">
+    <div class="canadaprovinces__wrap">
+        <ul class="canadaprovinces__list">
+            <?php foreach ($terms as $term){ ?>
+            <li class="canadaprovinces__item"><a class="canadaprovinces__link"href="<?php echo get_term_link( $term->slug, $term->taxonomy ); ?>"><?php echo $term->name; ?></a></li>
+            <?php } ?>
+        </ul>
+    </div>
+</div>
+    
