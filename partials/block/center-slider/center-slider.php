8a88c0c5f22d7bf1f6f2ac09b9ae209ee42a3643
Luis Colomé <hello@luiscolome.com>
2021-10-01 11:49:04 +0200

    Add options to select specialty and province on slider block

--- partials/block/center-slider/center-slider.php
+++ partials/block/center-slider/center-slider.php
@@ -7,15 +7,38 @@
 
 
 global $post;
+
+// We get Specialty custom field data.
+$specialty = get_field_object('br-specialties');
+$specialty_value = $specialty['value'];
+$specialty_label = $specialty['choices'][ $specialty_value ];
+
+// We get Proveinces custom field data.
+$province = get_field_object('br-provinces');
+$province_value = $province['value'];
+$province_label = $province['choices'][ $province_value ];
+
 $arg = array(
-    'post_type' => 'center', // this can be an array : array('guide','guide1',...)
-    'posts_per_page' => 99,
-    'order' => 'DESC',
-    'post_status' => 'publish'
+    'post_type'         => 'center', // this can be an array : array('guide','guide1',...)
+    'posts_per_page'    => 99,
+    'order'             => 'DESC',
+    'post_status'       => 'publish',
+    'tax_query'         => array(
+        'relation' => 'AND',
+        array(
+          'taxonomy'    => 'specialty',
+          'field'       => 'slug',
+          'terms'       => $specialty_value, // (the name of what you want to filter by (latest or whatever))
+        ),
+        array(
+            'taxonomy'    => 'province',
+            'field'       => 'slug',
+            'terms'       => $province_value, // (the name of what you want to filter by (latest or whatever))
+        )),
 );
     $query = new WP_Query($arg);
     if ( $query->have_posts() ) : ?>
-
+        <h4 class="swiper__title"><?php echo $specialty_label ?></h4>
         <div class="swiper mySwiper">
             <div class="swiper-wrapper">
                 <?php while ( $query->have_posts() ) : $query->the_post(); ?>
