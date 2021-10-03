e37926eb4f536e4f9b3a1b2909fed113b7633f57
Luis Colomé <hello@luiscolome.com>
2021-10-03 17:10:52 +0200

    Update header structure on Center template

--- single-center.php
+++ single-center.php
@@ -52,44 +52,53 @@ function lcm_entry_header(){
         <div class="lcm-header__columns">
 
             <div class="lcm-header__column">
-                <?php 
-                $args = array('number' => '1',);
-                $terms = get_terms('specialty', $args ); 
-                foreach($terms as $term) {
-                    // Get the $term link.
-                    $term_link = get_term_link( $term );
 
-                    echo '<a href="' . esc_url( $term_link ) . '">' . $term->name . '</a>';
-                } ?>
+                <div class="lcm-header__specialty">
 
-                <?php 
-                /*
-                * Star rating - https://wpdevdesign.com/acf-star-rating-in-wordpress/
-                */
-                $rating = get_field( 'br_star_rating', $post->ID );
-                if ( $rating ) {
-                    $average_stars = round( $rating * 2 ) / 2;
-                    $drawn = 5;
-                    echo '<div class="lcm-center__rating">';
-                        // full stars.
-                        for ( $i = 0; $i < floor( $average_stars ); $i++ ) {
-                        $drawn--;
-                            echo '<?xml version="1.0" ?><svg id="Layer_1" style="enable-background:new 0 0 30 30;" version="1.1" fill="#FFCE2C" width="20" height="20" viewBox="0 0 30 30" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M15.765,2.434l2.875,8.512l8.983,0.104c0.773,0.009,1.093,0.994,0.473,1.455l-7.207,5.364l2.677,8.576  c0.23,0.738-0.607,1.346-1.238,0.899L15,22.147l-7.329,5.196c-0.63,0.447-1.468-0.162-1.238-0.899l2.677-8.576l-7.207-5.364  c-0.62-0.461-0.3-1.446,0.473-1.455l8.983-0.104l2.875-8.512C14.482,1.701,15.518,1.701,15.765,2.434z"/></svg>';
-                        }
-                    echo '</div>';
-                } ?>
-                <h1 class="lcm-header__title entry-title" itemprop="headline"><?php the_title() ?></h1>
-                
-                <?php 
-                // We get the city
-                $cities = get_terms('city');
-                $city = $cities[0];
+                    <?php 
+                    $args = array('number' => '1',);
+                    $terms = get_terms('specialty', $args ); 
+                    foreach($terms as $term) {
+                        // Get the $term link.
+                        $term_link = get_term_link( $term );
 
-                // We get the province
-                $provinces = get_terms('province'); 
-                $province = $provinces[0]; ?>
+                        echo '<a href="' . esc_url( $term_link ) . '">' . $term->name . '</a>';
+                    } ?>
 
-                <p class="lcm-header__location"><?php echo $city->name ?>, <?php echo $province->name ?></p>
+                </div> <!-- End lcm-header__specialty -->
+
+                <div class="lcm-header__info">
+
+                    <?php 
+                    /*
+                    * Star rating - https://wpdevdesign.com/acf-star-rating-in-wordpress/
+                    */
+                    $rating = get_field( 'br_star_rating', $post->ID );
+                    if ( $rating ) {
+                        $average_stars = round( $rating * 2 ) / 2;
+                        $drawn = 5;
+                        echo '<div class="lcm-center__rating">';
+                            // full stars.
+                            for ( $i = 0; $i < floor( $average_stars ); $i++ ) {
+                            $drawn--;
+                                echo '<?xml version="1.0" ?><svg id="Layer_1" style="enable-background:new 0 0 30 30;" version="1.1" fill="#FFCE2C" width="20" height="20" viewBox="0 0 30 30" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M15.765,2.434l2.875,8.512l8.983,0.104c0.773,0.009,1.093,0.994,0.473,1.455l-7.207,5.364l2.677,8.576  c0.23,0.738-0.607,1.346-1.238,0.899L15,22.147l-7.329,5.196c-0.63,0.447-1.468-0.162-1.238-0.899l2.677-8.576l-7.207-5.364  c-0.62-0.461-0.3-1.446,0.473-1.455l8.983-0.104l2.875-8.512C14.482,1.701,15.518,1.701,15.765,2.434z"/></svg>';
+                            }
+                        echo '</div>';
+                    } ?>
+                    <h1 class="lcm-header__title entry-title" itemprop="headline"><?php the_title() ?></h1>
+                    
+                    <?php 
+                    // We get the city
+                    $cities = get_terms('city');
+                    $city = $cities[0];
+
+                    // We get the province
+                    $provinces = get_terms('province'); 
+                    $province = $provinces[0]; ?>
+
+                    <p class="lcm-header__location"><?php echo $city->name ?>, <?php echo $province->name ?></p>
+
+                </div><!-- End lcm-header__info -->
                
             </div><!-- End first column -->
 
