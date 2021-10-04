bfd6930756cdaa1b40d733301cd33154071d5681
Luis Colomé <hello@luiscolome.com>
2021-10-04 12:31:58 +0200

    Add styles for custom header on Center Template

--- single-center.php
+++ single-center.php
@@ -62,7 +62,7 @@ function lcm_entry_header(){
                         // Get the $term link.
                         $term_link = get_term_link( $term );
 
-                        echo '<a href="' . esc_url( $term_link ) . '">' . $term->name . '</a>';
+                        echo '<a class="lcm-header__specialty__link" href="' . esc_url( $term_link ) . '">' . $term->name . '</a>';
                     } ?>
 
                 </div> <!-- End lcm-header__specialty -->
@@ -77,11 +77,11 @@ function lcm_entry_header(){
                     if ( $rating ) {
                         $average_stars = round( $rating * 2 ) / 2;
                         $drawn = 5;
-                        echo '<div class="lcm-center__rating">';
+                        echo '<div class="lcm-header__rating">';
                             // full stars.
                             for ( $i = 0; $i < floor( $average_stars ); $i++ ) {
                             $drawn--;
-                                echo '<?xml version="1.0" ?><svg id="Layer_1" style="enable-background:new 0 0 30 30;" version="1.1" fill="#FFCE2C" width="20" height="20" viewBox="0 0 30 30" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M15.765,2.434l2.875,8.512l8.983,0.104c0.773,0.009,1.093,0.994,0.473,1.455l-7.207,5.364l2.677,8.576  c0.23,0.738-0.607,1.346-1.238,0.899L15,22.147l-7.329,5.196c-0.63,0.447-1.468-0.162-1.238-0.899l2.677-8.576l-7.207-5.364  c-0.62-0.461-0.3-1.446,0.473-1.455l8.983-0.104l2.875-8.512C14.482,1.701,15.518,1.701,15.765,2.434z"/></svg>';
+                                echo '<?xml version="1.0" ?><svg id="Layer_1" style="enable-background:new 0 0 30 30;" version="1.1" fill="#FFCE2C" width="34" height="34" viewBox="0 0 30 30" xml:space="preserve" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><path d="M15.765,2.434l2.875,8.512l8.983,0.104c0.773,0.009,1.093,0.994,0.473,1.455l-7.207,5.364l2.677,8.576  c0.23,0.738-0.607,1.346-1.238,0.899L15,22.147l-7.329,5.196c-0.63,0.447-1.468-0.162-1.238-0.899l2.677-8.576l-7.207-5.364  c-0.62-0.461-0.3-1.446,0.473-1.455l8.983-0.104l2.875-8.512C14.482,1.701,15.518,1.701,15.765,2.434z"/></svg>';
                             }
                         echo '</div>';
                     } ?>
